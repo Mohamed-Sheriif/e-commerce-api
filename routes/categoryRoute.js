129fedfe8590900eaa31866419e7d5cfe89f30b5
Mohamed-Sheriif <mohamedsh2782@gmail.com>
2024-09-24 12:46:00 +0300

    add express validator and validate category module with it

--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -7,15 +7,24 @@ const {
   updateCategory,
   deleteCategory,
 } = require("../services/categoryService");
+const {
+  getCategoryValidator,
+  createCategoryValidator,
+  updateCategoryValidator,
+  deleteCategoryValidator,
+} = require("../utils/validators/categoryValidator");
 
 const router = express.Router();
 
-router.route("/").post(createCategory).get(getAllCategories);
+router
+  .route("/")
+  .post(createCategoryValidator, createCategory)
+  .get(getAllCategories);
 
 router
   .route("/:id")
-  .get(getCategory)
-  .put(updateCategory)
-  .delete(deleteCategory);
+  .get(getCategoryValidator, getCategory)
+  .put(updateCategoryValidator, updateCategory)
+  .delete(deleteCategoryValidator, deleteCategory);
 
 module.exports = router;
