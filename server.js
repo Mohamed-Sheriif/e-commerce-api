66937ee89207e982bac71c41143d8ec94da5bdf5
Mohamed-Sheriif <mohamedsh2782@gmail.com>
2024-09-24 12:14:19 +0300

    add error handling middleware for express and non-express rejections

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const morgan = require("morgan");
 const dotnev = require("dotenv");
 
 // Routes
+const ApiError = require("./utils/apiError");
+const globalError = require("./middlewares/errorMiddleware");
 const dbConnection = require("./config/database");
 const CategoryRoute = require("./routes/categoryRoute");
 
 dotnev.config({ path: "config.env" });
-const PORT = process.env.PORT || 3000;
 const app = express();
 
 // Database Connection
@@ -23,6 +24,23 @@ app.use(express.json());
 // Mounted Routed
 app.use("/api/v1/categories", CategoryRoute);
 
-app.listen(PORT, () => {
+app.use("*", (req, res, next) => {
+  next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
+});
+
+// Global Error Handling Middleware For Express
+app.use(globalError);
+
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`);
+});
+
+// Handling Errors Outside Express
+process.on("unhandledRejection", (err) => {
+  console.error(`UnhandledRejection Error: ${err.name} | ${err.message}`);
+  server.close(() => {
+    console.error("Shuting down......");
+    process.exit(1);
+  });
 });
--- services/categoryService.js
+++ services/categoryService.js
@@ -1,6 +1,7 @@
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 
+const ApiError = require("../utils/apiError");
 const Category = require("../models/categoryModel");
 
 // @desc    Create New Category
@@ -30,13 +31,13 @@ exports.getAllCategories = asyncHandler(async (req, res) => {
 // @desc    Get Specific Category
 // @Route   GET /api/v1/categories/:id
 // @access  Public
-exports.getCategory = asyncHandler(async (req, res) => {
+exports.getCategory = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const category = await Category.findById(id);
 
   // Check if category exist
   if (!category) {
-    res.status(404).json({ msg: `No category found with the id ${id}` });
+    return next(new ApiError(`No category found with the id ${id}`, 404));
   }
 
   res.status(200).json({ data: category });
@@ -45,7 +46,7 @@ exports.getCategory = asyncHandler(async (req, res) => {
 // @desc    Update Category
 // @Route   PUT /api/v1/categories/:id
 // @access  Private
-exports.updateCategory = asyncHandler(async (req, res) => {
+exports.updateCategory = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
   const name = req.body.name;
 
@@ -57,7 +58,7 @@ exports.updateCategory = asyncHandler(async (req, res) => {
 
   // Check if category exist
   if (!category) {
-    res.status(404).json({ msg: `No category found with the id ${id}` });
+    return next(new ApiError(`No category found with the id ${id}`, 404));
   }
 
   res.status(200).json({ data: category });
@@ -66,14 +67,14 @@ exports.updateCategory = asyncHandler(async (req, res) => {
 // @desc    Delete Category
 // @Route   DELETE /api/v1/categories/:id
 // @access  Private
-exports.deleteCategory = asyncHandler(async (req, res) => {
+exports.deleteCategory = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
 
   const category = await Category.findByIdAndDelete({ _id: id });
 
   // Check if category exist
   if (!category) {
-    res.status(404).json({ msg: `No category found with the id ${id}` });
+    return next(new ApiError(`No category found with the id ${id}`, 404));
   }
 
   res.status(204).send();
