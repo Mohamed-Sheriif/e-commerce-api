f444670c905566b29bc0b2983fb2187b6ae24bc0
Mohamed-Sheriif <mohamedsh2782@gmail.com>
2024-09-26 12:31:04 +0300

    implement product model

---
+++ models/productModel.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Product title is required!"],
+      trim: true,
+      minlength: [3, "Too short product title!"],
+      maxlength: [100, "Too long product title!"],
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: [true, "Product description is required!"],
+      trim: true,
+      minlength: [20, "Too short product description!"],
+    },
+    quantity: {
+      type: Number,
+      required: [true, "Product quantity is required!"],
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, "Product price is required!"],
+      trim: true,
+      max: [20, "Too long product price!"],
+    },
+    priceAfterDiscount: {
+      type: Number,
+    },
+    colors: [String],
+    images: [String],
+    coverImage: {
+      type: String,
+      required: [true, "Product cover image is required!"],
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Category",
+      required: [true, "Product must belong to category!"],
+    },
+    subCategory: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "SubCategory",
+      },
+    ],
+    brand: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Brand",
+    },
+    averageRating: {
+      type: Number,
+      min: [1, "Rating must be at least 1"],
+      max: [5, "Rating must can not be more than 5"],
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
